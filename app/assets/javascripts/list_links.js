a31527df8316c32917423a2de942df71698e5f4e
Bryan Goss <bcgoss@gmail.com>
2016-12-19 21:37:42 -0700

    Remove lodash

--- app/assets/javascripts/list_links.js
+++ app/assets/javascripts/list_links.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
-  _.forEach( $.get('/api/v1/links'), setup(link) )
+  //_.forEach( $.get('/api/v1/links'), setup(link) )
 })
 
-function setup(link){
-  renderLink(link.id)
-  attachEvents(link)
-}
+// function setup(link){
+//   renderLink(link.id)
+//   attachEvents(link)
+// }
 
 function attachEvents(raw_link) {
   debugger
