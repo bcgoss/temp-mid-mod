a1cc96d74d80bafeb7db2a63ec0253e5c18a0f14
Bryan Goss <bcgoss@gmail.com>
2016-12-19 18:06:48 -0700

    Add relationship between user and link

--- app/models/link.rb
+++ app/models/link.rb
@@ -4,6 +4,7 @@ class Link < ActiveRecord::Base
 
   validates :url, url: true
 
+  belongs_to :user
   def read!
     publish if read == false
     update_attribute(:read, true)
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
   has_secure_password
   validates_uniqueness_of :email
   validates_confirmation_of :password
+  has_many :links
 end
